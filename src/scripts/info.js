a4ba60e62679a8c5d86f745293f64540956f0f65
Luis Meyer <luis_meyer@outlook.de>
2020-04-29 11:06:57 +0200

    Add custom domain

--- src/scripts/info.js
+++ src/scripts/info.js
@@ -2,15 +2,15 @@ const bot = require("../bot");
 const secrets = require("../../secrets/secrets.json");
 
 (async () => {
-  const url = bot.requestUrl(secrets.token)("setWebhook");
+  const url = bot.requestUrl(secrets.token)("getWebhookInfo");
   const res = await bot.fetch(url, {
     method: "post",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      url: secrets.webhookUrl
-    })
-  })
+      url: secrets.webhookUrl,
+    }),
+  });
   console.info(res);
 })();
