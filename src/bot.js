61239acd0eac2d443e74880de80e1783887f9218
Luis Meyer <luis_meyer@outlook.de>
2020-04-30 11:34:31 +0200

    Updates dependencies

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,11 @@ const nodeFetch = require("node-fetch");
 
 const { BOT_TOKEN, IS_LOCAL } = process.env;
 
-module.exports.requestUrl = token => method =>
+module.exports.requestUrl = (token) => (method) =>
   `https://api.telegram.org/bot${token}/${method}`;
 
 module.exports.fetch = (url, params) =>
-  nodeFetch(url, params).then(res => res.json());
+  nodeFetch(url, params).then((res) => res.json());
 
 const telegramUrl = this.requestUrl(BOT_TOKEN);
 const telegramFetch = (method, params) =>
@@ -22,11 +22,11 @@ module.exports.sendMessage = (chat, message) => {
   return telegramFetch("sendMessage", {
     method: "post",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       chat_id: chat,
-      text: message
-    })
+      text: message,
+    }),
   });
 };
